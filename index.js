991ccbde882c3f9f5bad6217b2a3a592b830de0d
rico2290 <rjl220990@gmail.com>
2018-11-10 02:30:43 -0200

    corrigindo post

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-const express = require('express')
-const axios = require('axios')
+const express = require ('express')
 const app = express()
-const bodyParser = require('body-parser')
+const axios = require ('axios')
+const bodyparser = require ('body-parser')
 
-app.set('view engine','ejs' )
-app.use(bodyParser.urlencoded({extended: true}))
 
+app.set('view engine', 'ejs')
+app.use(bodyparser.urlencoded({extended:true}))
 
 const port = process.env.port || 3000 
 
+//let i = 10
 app.get('/', async(request, response) => {
     const content = await axios.get('https://fullstacklab-6b9bb.firebaseio.com/teste.json')
+    await axios.post('https://fullstacklab-6b9bb.firebaseio.com/categorias.json',{categoria: 'Receitas'})
     console.log(content.data)
-    response.render('index', {i: content.data})
+    response.render('index',{i: content.data})
 })
-//renderizar para nova pagina
+
 app.get('/categorias/nova', (req, res)=>{
     res.render('categorias/nova')
 })
-
 app.post('/categorias/nova', async(req, res)=>{
+    await axios.post('https://fullstacklab-6b9bb.firebaseio.com/categorias.json',{categoria: req.body.categoria})
     //console.log(req.body)
-    await axios.post('https://fullstacklab-6b9bb.firebaseio.com/categorias.json', {
-        categoria: req.body.categoria})
     res.send(req.body)
 })
- 
-
-app.listen(port, (erro) => {
-    if(erro){
-        console.log('Ops! deu erro')
-    }else{
-        console.log('FullStackLab server is running on port:',port)
+app.listen(port, (error)=>{
+    if (error) {
+        console.log('Opá!!! deu erro')
     }
+    console.log('FullStack Server is running on port:',port)
 })
